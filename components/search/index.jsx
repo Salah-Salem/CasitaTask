c94c647ab88d7fa037eb716ca009d8b6e4fba550
Salah Salem <salah.salem@casita.com>
2023-10-01 00:42:52 +0300

    edit some fixes

--- components/search/index.jsx
+++ components/search/index.jsx
@@ -1,17 +1,18 @@
 import { Fragment } from 'react'
 import { BiSearchAlt2 } from 'react-icons/bi'
 export default function Search({ onSearch }) {
-  return (
-    <Fragment>
-      <div className="form-control">
-        <input
-          type="text"
-          name="searchInput"
-          id="searchInput"
-          onChange={(e) => onSearch(e.target.value)}
-        />
-        <BiSearchAlt2 />
-      </div>
-    </Fragment>
-  )
+    return (
+        <Fragment>
+            <div className="form-control">
+                <input
+                    type="text"
+                    name="searchInput"
+                    id="searchInput"
+                    placeholder='   Search by city name'
+                    onChange={(e) => onSearch(e.target.value)}
+                />
+                <BiSearchAlt2 />
+            </div>
+        </Fragment>
+    )
 }
